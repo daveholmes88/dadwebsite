3a2c2d32f8a151efaeea7290d4d7ee5b09a0c845
daveholmes88 <github email address>
2020-12-01 12:26:58 -0600

    contact fix

--- src/components/Contact/ContactBruce.js
+++ src/components/Contact/ContactBruce.js
@@ -10,7 +10,9 @@ export default function ContactBruce() {
             <Row>
                 <ContactIndex />
                 <Col>
-                    <h1>My first name at Brucetholmes.com</h1>
+                    <h1>Contact Bruce</h1>
+                    <p>My email address is my first name at BruceTHolmes.com</p>
+                    <p>Bruce Holmes<br></br>1115 Mulford Street<br></br>Evanston, IL 60202-3318</p>
                 </Col>
             </Row>
         </Container>
