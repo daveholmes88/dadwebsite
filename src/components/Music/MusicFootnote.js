7b15bfe8be92d10941ce0ee46926af148aa145f7
daveholmes88 <github email address>
2021-05-08 17:30:52 -0500

    feldenkrais download

--- src/components/Music/MusicFootnote.js
+++ src/components/Music/MusicFootnote.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Col, Image, Row } from 'react-bootstrap';
+import { Col, Image, Row } from 'react-bootstrap';
 
 export default function MusicFootnote() {
 
