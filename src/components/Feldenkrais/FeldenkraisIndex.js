59f1629d7a8e83e51aaf8af176b84b6bde36a588
Dave Holmes <daveholmes88@gmail.com>
2022-05-11 12:29:29 -0500

    updated feldenkrais buttons

--- src/components/Feldenkrais/FeldenkraisIndex.js
+++ src/components/Feldenkrais/FeldenkraisIndex.js
@@ -30,8 +30,12 @@ export default function FeldenkraisIndex() {
                     <br></br><br></br>
                     <a href='/Feldenkrais/MosheFeldenkrais'>Moshe’s Healing Touch from Runner's World</a>
                     <br></br><br></br>
-                    <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" />
-
+                    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                        <input type="hidden" name="cmd" value="_s-xclick" />
+                        <input type="hidden" name="hosted_button_id" value="XR6RB7D8XWUMY" />
+                        <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" />
+                        <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+                    </form>
                 </Container>
             </Card>
         </Col>
--- src/components/Feldenkrais/FeldenkraisTitle.js
+++ src/components/Feldenkrais/FeldenkraisTitle.js
@@ -10,7 +10,12 @@ export default function FeldenkraisTitle() {
             <br></br>
             <h3 className='text-center'>A 48 lesson home study course for $48</h3>
             <div className='text-right'>
-                <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" />
+                <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                    <input type="hidden" name="cmd" value="_s-xclick" />
+                    <input type="hidden" name="hosted_button_id" value="XR6RB7D8XWUMY" />
+                    <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" />
+                    <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+                </form>
             </div>
         </Container>
     )
