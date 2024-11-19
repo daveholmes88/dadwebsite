04aa35e8702c802092177b2b7933c76eb8c47e95
Dave Holmes <daveholmes88@gmail.com>
2024-11-19 15:12:06 -0600

    update becoming cover photo

--- src/components/Becoming/Becoming.js
+++ src/components/Becoming/Becoming.js
@@ -16,10 +16,10 @@ export default function Becoming() {
                     <h4 className='text-center'>This is the book I wish I had when I was learning to call.</h4>
                     <Image
                         alt='becomingCover'
-                        src='/Becoming/cover.jpeg'
+                        src='/Becoming/BecomingCover.png'
                         fluid='true'
                     />
-                    <h4>4th Edition</h4>
+                    <h4>5th Edition</h4>
                     <h4>8.5 x 11" 206 pages</h4>
                     <p>wire spiral bound so it can lie flat</p>
                     <p>Includes a pdf file of pages for mobile study</p>
