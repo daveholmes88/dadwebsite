9c01abee0207489418d1698fc56730a4d660dc1c
Dave Holmes <daveholmes88@gmail.com>
2025-01-08 19:45:04 -0600

    Europe info

--- src/components/Becoming/BecomingDownload.js
+++ src/components/Becoming/BecomingDownload.js
@@ -9,6 +9,7 @@ export default function BecomingDownload() {
             <a href='/becoming.zip' download><Button >DOWNLOAD THE PDFs WITH THIS BUTTON!!!!</Button></a>
             <br></br><br></br>
             <h3>If you have any issues please email my son Dave at DaveHolmes88@gmail.com</h3>
+            <h3>I've heard from several sources that those in Europe are not able to download the file. I'm working to figure out the problem. In the mean time please just email my son and he will send you the file. I'm sorry for the inconvenience.</h3>
         </Container>
     )
 }
